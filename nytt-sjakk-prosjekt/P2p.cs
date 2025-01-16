fb4fee82a925b6dbdbc7de6b843c0d2ac8c2a869
oso082024 <oso082024@gmail.com>
2025-01-16 14:47:56 +0100

    Started working on p2p
    Looking at different solutions for this, started testing out a few locally.

---
+++ nytt-sjakk-prosjekt/P2p.cs
@@ -0,0 +1,38 @@
+using Godot;
+using System;
+using System.Net;
+using System.Net.Sockets;
+
+
+
+public partial class P2p : Node
+{
+	//// Called when the node enters the scene tree for the first time.
+	//public override void _Ready()
+	//{
+	//}
+//
+	//// Called every frame. 'delta' is the elapsed time since the previous frame.
+	//public override void _Process(double delta)
+	//
+	
+	private TcpListener _tcpListener;
+	
+	public TCPServer()
+	{
+		
+	}
+	
+	private void StartServer()
+	{
+		int port = 40404;
+		string hostAdress = IPAdress.Parse(127.0.0.1); // Localhost. Change to actual IP? Can the program find it?
+		_tcpListener = new TCPListener(hostAdress, port); // creates listener using port and hostAdress
+		_tcpListener.start(); // starts listener created above
+		
+		byte[] buffer = new byte[256];
+		string receivedMessage; // TODO: Endre til noe mer fornuftig, so f.eks. "opponentsMove";
+		
+		//adding parsed data loop in the future here.
+	}
+}
