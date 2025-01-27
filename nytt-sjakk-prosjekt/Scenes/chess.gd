6970c50b5331f41c02ff04023296c596e10a1a04
Joachim Åsen Kvamme <150397418+JoachimKvamme@users.noreply.github.com>
2025-01-27 15:02:52 +0100

    Cleanup

--- nytt-sjakk-prosjekt/Scenes/chess.gd
+++ nytt-sjakk-prosjekt/Scenes/chess.gd
@@ -212,36 +212,6 @@ func play_from_input(from : String, to : String):
 		var1 = parsed_move(to).y
 		set_moves(var2, var1)
 
-func flip_trough_moves(game_state : Array):
-	for child in pieces.get_children():
-		child.queue_free()
-	
-	for i in BOARD_SIZE:
-		for j in BOARD_SIZE:
-			var holder = TEXTURE_HOLDER.instantiate()
-			pieces.add_child(holder)
-			holder.global_position = Vector2(j * CELL_WIDHT + (CELL_WIDHT / 2), -i * CELL_WIDHT - (CELL_WIDHT / 2))
-			
-			match board[i][j]:
-				-6: holder.texture = BLACK_KING
-				-5: holder.texture = BLACK_QUEEN
-				-4: holder.texture = BLACK_ROOK
-				-3: holder.texture = BLACK_BISHOP
-				-2: holder.texture = BLACK_KNIGHT
-				-1: holder.texture = BLACK_PAWN
-				0: holder.texture = null
-				6: holder.texture = WHITE_KING
-				5: holder.texture = WHITE_QUEEN
-				4: holder.texture = WHITE_ROOK
-				3: holder.texture = WHITE_BISHOP
-				2: holder.texture = WHITE_KNIGHT
-				1: holder.texture = WHITE_PAWN
-				
-	if white: turn.texture = TURN_WHITE
-	else: turn.texture = TURN_BLACK
-
-	
-
 func reset():
 	white = true
 	state = false
